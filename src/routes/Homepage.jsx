5670e991d881293b57f6f7c754b261a21ad9405c
Biswayan-Roy9 <2023cse14biswayan@buie.ac.in>
2026-01-11 10:21:18 +0530

    fixed typo in write path in Homepage.jsx

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -25,7 +25,7 @@ function Homepage() {
             </p>
         </div>
         {/* animated button */}
-        <Link to="write" className="hidden md:block relative">
+        <Link to="/write" className="hidden md:block relative">
           <svg
             viewBox="0 0 200 200"
             width="200"
